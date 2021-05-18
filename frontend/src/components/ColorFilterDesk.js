46f5ec01ce013c0ec0afed14da1191e3c1a76895
johnakisk0700 <johnnstavv@gmail.com>
2021-05-18 22:30:25 +0300

    otinani

--- frontend/src/components/ColorFilterDesk.js
+++ frontend/src/components/ColorFilterDesk.js
@@ -3,7 +3,7 @@ const ColorFilterDesk = ({filterByColor, xrwmataFiltra}) => {
     const tickColorSwap = `url("data:image/svg+xml,%3Csvg width='1.5rem' height='1.5rem' viewBox='0 0 1rem 1rem' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.414 11L4 12.414l5.414 5.414L20.828 6.414 19.414 5l-10 10z' fill='black' fill-rule='nonzero'/%3E%3C/svg%3E")`
     return (
         <div className='markes-filter-desk'>
-            <h3 className='text-white m-3'>Χρώματα</h3>
+            <h3 className='text-black m-3'>Χρώματα</h3>
             <div className='markes-wrapper-desk'> 
             {Object.entries(xrwmata).map(([key, val], i) => (
                     <div className={xrwmataFiltra.includes(key) ? 'dokimastikoGroup-selected' : 'dokimastikoGroup'} style={xrwmataFiltra.includes(key) ? {backgroundColor: `${key}`} : {}}>
